e3e8f5f8bde1ed38e56c6c318f858ef851202c20
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-05-22 16:16:29 +0530

    change in

--- gallery.js
+++ gallery.js
@@ -3,7 +3,6 @@ const cont= document.querySelectorAll(".cont");
 const observer = new IntersectionObserver(
     entries => {
         entries.forEach(entry => {
-            console.log(11);
             entry.target.classList.toggle("show",entry.isIntersecting);
             if(entry.isIntersecting) observer.unobserve(entry.target)
         })
@@ -24,34 +23,34 @@ cont.forEach(cont => {
         }
       };
     async function loader(){
-        await fetch("https://dhanush-image-generator-api.netlify.app/", configObj)
+        await fetch("https://image-generator-api.netlify.app/", configObj)
     
         .then(async function (response) {
           return await response.json();
         })
         .then(async function (object)
         {
+            let json=object.body;
+            var a=document.getElementById("body-container").innerHTML;
             for(let i = 0; i < json.length; i++) {
                 let obj = json[i];
             
-                console.log(obj.id);
-                let a= `<div class="cont">
+              
+                 a+= `<div class="cont">
                 <img
-                  src="https://images.unsplash.com/photo-1527049979667-990f1d0d8e7f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80"
+                  src="${obj.image}"
                   alt=""
                   height="300px"
                   srcset=""
                   class="image"
                 />
                 <div class="cont-disc">
-                  <div class="cont-disc-name">NAME</div>
+
+44                <div class="cont-disc-name">${obj.title}</div>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
-                    natus suscipit pariatur excepturi omnis quam sapiente ducimus
-                    inventore distinctio minus aliquid modi voluptates quod, accusamus
-                    nesciunt voluptate quia. Accusantium, non!
+                  ${obj.description}
                   </p>
-                  <h4>by yamuna</h4>
+                  <h4>by ${obj.name}</h4>
         
                   <div class="btn-container">
                     <div href="" class="button1">♡</div>
