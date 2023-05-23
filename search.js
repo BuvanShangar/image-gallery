f9ac2f15c1f66d569f1c348e2d23e0a5545ca261
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-05-23 15:36:24 +0530

    change in download

--- search.js
+++ search.js
@@ -1,4 +1,3 @@
-
 const cont= document.querySelectorAll(".cont");
 const observer = new IntersectionObserver(
     entries => {
@@ -22,8 +21,8 @@ cont.forEach(cont => {
           "Content-Type": "application/json",
         }
       };
-    async function loader(){
-      document.getElementById("body-container").innerHTML+=`<div class="loader" id="loader">
+    async function search(){
+      document.getElementById("body-container").innerHTML=`<div class="loader" id="loader">
                           <div class="log-img" style="background-color: rgba(217, 217, 217, 0.666); border-radius: 24px;">
                           <img src="./Screenshot from 2023-05-08 18-46-14.png" alt="" sizes="" width="300px" srcset="" class=""><br>
                           <img src="https://i.gifer.com/YCZH.gif" alt="" sizes="" width="300px" srcset="" class="">
@@ -47,35 +46,34 @@ cont.forEach(cont => {
                 //console.log("hello");
               //console.log(json);
               document.getElementById("body-container").innerHTML+= `<div class="cont">
-              <img
-                src="${obj.image}"
-                alt=""
-                height="300px"
-                srcset=""
-                class="image"
-              />
-              <div class="cont-disc">
-              <div class="cont-disc-name">${obj.title}</div>
-                <p>
-                ${obj.description}
-                </p>
-                <h4>by ${obj.name}</h4>
-      
-                <div class="btn-container">
-                  <div href="" class="button1">♡</div>
-                  <a href="${obj.image}" download="${obj.title}.jpg" class="button12">
-                    <img
-                      src="https://img.icons8.com/?size=512&id=Ezk6WeFucgyE&format=png"
-                      width="25px"
-                      height="25px"
-                    />
-                  </a>
-                  <div href="" class="button1">➦</div>
+                <img
+                  src="${obj.image}"
+                  alt=""
+                  height="300px"
+                  srcset=""
+                  class="image"
+                />
+                <div class="cont-disc">
+                <div class="cont-disc-name">${obj.title}</div>
+                  <p>
+                  ${obj.description}
+                  </p>
+                  <h4>by ${obj.name}</h4>
+        
+                  <div class="btn-container">
+                    <div href="" class="button1">♡</div>
+                    <a href="${obj.image}" download="${obj.title}.jpg" class="button12">
+                      <img
+                        src="https://img.icons8.com/?size=512&id=Ezk6WeFucgyE&format=png"
+                        width="25px"
+                        height="25px"
+                      />
+                    </a>
+                    <div href="" class="button1">➦</div>
+                  </div>
                 </div>
-              </div>
-            </div>`;
+              </div>`;
               console.log("end");
              }
 
     })};
-     
