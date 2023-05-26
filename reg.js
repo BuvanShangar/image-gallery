34bf9153c063ff3f702310c8d8d232bad389f376
dhanush <dhanushprabakaran18@gmail.com>
2023-05-26 18:45:32 +0530

    changes

--- reg.js
+++ reg.js
@@ -7,10 +7,13 @@
     if (username == "" || password == "" || confirmPassword == "" || email == "") {
         alert("All fields are required!");
         return false;
-      }
-
-      if (password !== confirmPassword) {
+      }else if (password !== confirmPassword) {
         alert("Password and confirm password do not match!");
         return false;
+      }else{
+        register();
       }
-}
+};
+asyc function register(){
+
+};
