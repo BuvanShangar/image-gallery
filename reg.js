0240847640929a6c3beb6e56abaffd11f21a8834
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-05-26 11:10:46 +0530

    login

---
+++ reg.js
@@ -0,0 +1,16 @@
+ function validateForm() {
+    var username = document.getElementById("Uname").value;
+    var password = document.getElementById("pwd").value;
+    var confirmPassword = document.getElementById("cpwd").value;
+    var email = document.getElementById("em").value;
+
+    if (username == "" || password == "" || confirmPassword == "" || email == "") {
+        alert("All fields are required!");
+        return false;
+      }
+
+      if (password !== confirmPassword) {
+        alert("Password and confirm password do not match!");
+        return false;
+      }
+}
--- search.js
+++ search.js
@@ -16,7 +16,7 @@ cont.forEach(cont => {
 async function search(){
       let button = document.getElementById("button").value;
       let formData = {
-        title: `${button}`
+        "search":`${button}`
       };
       let configObj = {
         method: "POST",
@@ -33,50 +33,50 @@ async function search(){
                           </div>
                         </div>`;
 
-        await fetch("https://image-gallery-api-git.netlify.app/get/", configObj)
+        await fetch("https://image-gallery-api-git.netlify.app/get", configObj)
         .then(async function (response) {
           console.log(response);
           return await response.json();
         })
         .then(async function (object)
         {    console.log(object);
-            // let json=JSON.parse(object);
-            // console.log(json);
-            // var a=document.getElementById("body-container").innerHTML="";
-            // for(let i = 0; i < json.length; i++) {
-            //     let obj = json[i];
-            //     console.log(obj.image);
-            //     //console.log("hello");
-            //   //console.log(json);
-            //   document.getElementById("body-container").innerHTML+= `<div class="cont">
-            //     <img
-            //       src="${obj.image}"
-            //       alt=""
-            //       height="300px"
-            //       srcset=""
-            //       class="image"
-            //     />
-            //     <div class="cont-disc">
-            //     <div class="cont-disc-name">${obj.title}</div>
-            //       <p>
-            //       ${obj.description}
-            //       </p>
-            //       <h4>by ${obj.name}</h4>
-        
-            //       <div class="btn-container">
-            //         <div href="" class="button1">♡</div>
-            //         <a href="${obj.image}" download="${obj.title}.jpg" class="button1">
-            //           <img
-            //             src="https://img.icons8.com/?size=512&id=Ezk6WeFucgyE&format=png"
-            //             width="25px"
-            //             height="25px"
-            //           />
-            //         </a>
-            //         <div href="" class="button1">➦</div>
-            //       </div>
-            //     </div>
-            //   </div>`;
+            let json=object;
+            console.log(json);
+            var a=document.getElementById("body-container").innerHTML="";
+            for(let i = 0; i < json.length; i++) {
+                let obj = json[i];
+                console.log(obj.image);
+               // console.log("hello");
+              console.log(json);
+              document.getElementById("body-container").innerHTML+= `<div class="cont">
+              <img
+                src="${obj.image}"
+                alt=""
+                height="300px"
+                srcset=""
+                class="image"
+              />
+              <div class="cont-disc">
+              <div class="cont-disc-name">${obj.title}</div>
+                <p>
+                ${obj.description}
+                </p>
+                <h4>by ${obj.name}</h4>
+      
+                <div class="btn-container">
+                  <div href="" class="button1">♡</div>
+                  <a href="${obj.image}" download="${obj.title}.jpg" class="button1">
+                    <img
+                      src="https://img.icons8.com/?size=512&id=Ezk6WeFucgyE&format=png"
+                      width="25px"
+                      height="25px"
+                    />
+                  </a>
+                  <div href="" class="button1">➦</div>
+                </div>
+              </div>
+            </div>`;
               console.log("end");
-             //}
+             }
 
     })};
