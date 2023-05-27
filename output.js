9640b52b4c2a61a6f18fa0b1ab429497b5e615b6
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-05-27 08:46:42 +0530

    changes

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function handleFileSelect(event) {
   const file = event.target.files[0];
   const reader = new FileReader();
@@ -74,80 +74,4 @@ async function fun(){
     await  console.log(error.message);
     });
 };
-=======
-function handleFileSelect(event) {
-  const file = event.target.files[0];
-  const reader = new FileReader();
 
-  reader.onload = function (e) {
-    var previewImage = document.getElementById("preview");
-    previewImage.style.visibility = "visible";
-    previewImage.style.height = "250px";
-    previewImage.style.width = "250px";
-    previewImage.src = e.target.result;
-  };
-
-  reader.readAsDataURL(file);
-}
-const fileInput = document.getElementById("uploadInput");
-fileInput.addEventListener("change", handleFileSelect, false);
-
-async function fun(){
-  console.log("hello");
-  var previewImage = document.getElementById("preview");
-  
-  previewImage.src ="";
-  previewImage.style.visibility = "hidden";
-  var element0 =await document.getElementById("loader");
-        element0.style.visibility = "visible" ;
-  var element1 =await document.getElementById("body-continer");
-        element1.style.visibility = "hidden" ;      
-
-  //await document.getElementsByClassName("loader").css('background-color', 'grey');
-  function ConvertToBase64(file) {
-    return new Promise((resolve, reject) => {
-      const filereader = new FileReader();
-      filereader.readAsDataURL(file);
-      filereader.onload = () => {
-        resolve(filereader.result);
-      };
-      filereader.onerror = (error) => {
-        reject(error);
-      };
-    });
-  }
-  const Nam = document.getElementById("name").value;
-  const Tit = document.getElementById("title").value;
-  const Img = document.getElementById("uploadInput");
-  const ImgName = await ConvertToBase64(Img.files[0]);
-  const Des = document.getElementById("Description").value;
-
-  let formData = {
-    "name": `${Nam}`,
-    "title": `${Tit}`,
-    "image": `${ImgName}`,
-    "description":`${Des}`
-  };
-  let configObj = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData)
-  };
-
- await fetch("https://image-gallery-api-git.netlify.app/post", configObj)
-
-    .then(async function (response) {
-      return await response.json();
-    })
-    .then(async function (object) {
-    // let aa= await document.getElementById("forms").reset();
-     await console.log(object);
-    })
-    .catch(async function (error) {
-     await alert("Bad things! Ragnarők!");
-    await  console.log(error.message);
-    });
-};
->>>>>>> 34bf9153c063ff3f702310c8d8d232bad389f376
