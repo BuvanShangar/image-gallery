0d258d32be4fc064aaf7e3de62351e6f5af9df9b
dhanush <dhanushprabakaran18@gmail.com>
2023-05-27 18:10:26 +0530

    changes

--- unsplash-page.js
+++ unsplash-page.js
@@ -20,58 +20,33 @@ cont.forEach(cont => {
         }
       };
     async function generate(){
-      document.getElementById("body-container").innerHTML=`<div class="loader" id="loader">
-                          <div class="log-img" style="background-color: rgba(217, 217, 217, 0.666); border-radius: 24px;">
-                          <img src="./Screenshot from 2023-05-08 18-46-14.png" alt="" sizes="" width="300px" srcset="" class=""><br>
-                          <img src="https://i.gifer.com/YCZH.gif" alt="" sizes="" width="300px" srcset="" class="">
-                          <!-- <h2 class="" style="text-align: center; color: #000000;" >loading...</h2> -->
-                          </div>
-                        </div>`;
+      var username = document.getElementById("search").value;      
+console.log(username);
+document.getElementById("body-container").innerHTML= `<div class="cont">
+<img
+  src="https://source.unsplash.com/random/?${username}"
+  alt=""
+  height="300px"
+  srcset=""
+  class="image"
+/>
+<div class="cont-disc">
+<div class="cont-disc-name">${username}</div>
+  <p>
+  ${username}
+  </p>
+  <h4>by ${username}</h4>
 
-        await fetch("https://image-gallery-api-git.netlify.app/", configObj)
-    
-        .then(async function (response) {
-          return await response.json();
-        })
-        .then(async function (object)
-        {    console.log("hello");
-            let json=JSON.parse( object.body);
-
-            var a=document.getElementById("body-container").innerHTML="";
-            for(let i = 0; i < json.length; i++) {
-                let obj = json[i];
-                console.log(obj.image);
-                //console.log("hello");
-              //console.log(json);
-              document.getElementById("body-container").innerHTML+= `<div class="cont">
-                <img
-                  src="${obj.image}"
-                  alt=""
-                  height="300px"
-                  srcset=""
-                  class="image"
-                />
-                <div class="cont-disc">
-                <div class="cont-disc-name">${obj.title}</div>
-                  <p>
-                  ${obj.description}
-                  </p>
-                  <h4>by ${obj.name}</h4>
-        
-                  <div class="btn-container">
-                    <div href="" class="button1">♡</div>
-                    <a href="${obj.image}" download="${obj.title}.jpg" class="button1">
-                      <img
-                        src="https://img.icons8.com/?size=512&id=Ezk6WeFucgyE&format=png"
-                        width="25px"
-                        height="25px"
-                      />
-                    </a>
-                    <div href="" class="button1">➦</div>
-                  </div>
-                </div>
-              </div>`;
-              console.log("end");
-             }
-
-    })};
+  <div class="btn-container">
+    <a href="https://source.unsplash.com/random/?${username}" download="${username}.jpg" class="button1">
+      <img
+        src="https://img.icons8.com/?size=512&id=Ezk6WeFucgyE&format=png"
+        width="25px"
+        height="25px"
+      />
+    </a>
+    <div href="" class="button1">➦</div>
+  </div>
+</div>
+</div>`
+};
