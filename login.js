30c32fa6b4f16b79eee68b512e5e9675b288c1bd
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-05-27 08:46:54 +0530

    changes

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function validateForm() {
     var email = document.forms["loginForm"]["username"].value;
     var password = document.forms["loginForm"]["password"].value;
@@ -54,61 +54,5 @@ async function login(){
 
 
 
-=======
-function validateForm() {
-    var email = document.forms["loginForm"]["username"].value;
-    var password = document.forms["loginForm"]["password"].value;
 
-    if (email == "") {
-      alert("Please enter your username.");
-      return false;
-    }else if (password == "") {
-      alert("Please enter your password.");
-      return false;
-    }else{
-      login();
-    }
-  }
-async function login(){
-  console.log("hello");
- 
-  var email = document.forms["loginForm"]["username"].value;
-  var password = document.forms["loginForm"]["password"].value;
-  let formData = {
-    "email": `${email}`,
-    "password": `${password}`
-  };
-  let configObj = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData)
-  };
-
- await fetch("https://image-gallery-api-git.netlify.app/login", configObj)
-
-    .then(async function (response) {
-      return await response.json();
-    })
-    .then(async function (object) {
-      
-      element0.style.visibility = "hidden" ;
-      element1.style.visibility = "visible" ;
-    let aa= await document.getElementById("forms").reset();
-      // await alert("uploaded");
-     await console.log(object);
-    })
-    .catch(async function (error) {
-      element0.style.opacity = "hidden";
-      element1.style.visibility = "visible" ;
-      let aa= await document.getElementById("forms").reset();
-     await alert("Bad things! Ragnarők!");
-    await  console.log(error.message);
-    });
-
-
-
-
->>>>>>> 34bf9153c063ff3f702310c8d8d232bad389f376
  }
--- reg.js
+++ reg.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
  function validateForm() {
     var username = document.getElementById("Uname").value;
     var password = document.getElementById("pwd").value;
@@ -17,24 +17,5 @@
 };
 asyc function register(){
 
-=======
- function validateForm() {
-    var username = document.getElementById("Uname").value;
-    var password = document.getElementById("pwd").value;
-    var confirmPassword = document.getElementById("cpwd").value;
-    var email = document.getElementById("em").value;
 
-    if (username == "" || password == "" || confirmPassword == "" || email == "") {
-        alert("All fields are required!");
-        return false;
-      }else if (password !== confirmPassword) {
-        alert("Password and confirm password do not match!");
-        return false;
-      }else{
-        register();
-      }
-};
-asyc function register(){
-
->>>>>>> 34bf9153c063ff3f702310c8d8d232bad389f376
 };
