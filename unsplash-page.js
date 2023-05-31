bb2ce197f0040d51bd31183fe3dabee6cf01cb95
dhanush <dhanushprabakaran18@gmail.com>
2023-05-31 20:04:34 +0530

    changes

--- unsplash-page.js
+++ unsplash-page.js
@@ -1,33 +1,35 @@
-const cont= document.querySelectorAll(".cont");
-const observer = new IntersectionObserver(
-    entries => {
-        entries.forEach(entry => {
-            entry.target.classList.toggle("show",entry.isIntersecting);
-            if(entry.isIntersecting) observer.unobserve(entry.target)
-        })
-    },
-    {
-        threshold: 0,
-    }
-)
-cont.forEach(cont => {
-        observer.observe(cont)
-    })
+// const cont= document.querySelectorAll(".cont");
+// const observer = new IntersectionObserver(
+//     entries => {
+//         entries.forEach(entry => {
+//             entry.target.classList.toggle("show",entry.isIntersecting);
+//             if(entry.isIntersecting) observer.unobserve(entry.target)
+//         })
+//     },
+//     {
+//         threshold: 0,
+//     }
+// )
+// cont.forEach(cont => {
+//         observer.observe(cont)
+//     })
     
-    async function generate(){
+
+async function generate(){
       var search = document.getElementById("search").value;      
 //console.log(username);
 let configObj = {
   method: "GET",
   headers: {
     "Content-Type": "application/json",
-  },
-  body:{
-    client_id:"Lit6CmPbDpMfc6suXVJ9IdPibYV3TTxpWcazLE8wBQM",
-    query:`${search}`,
-    count:15
-
   }
+  //,
+  // body:{
+  //   client_id:"Lit6CmPbDpMfc6suXVJ9IdPibYV3TTxpWcazLE8wBQM",
+  //   query:`${search}`,
+  //   count:15
+
+  // }
 };
 document.getElementById("body-container").innerHTML=`<div class="loader" id="loader">
                           <div class="log-img" style="background-color: rgba(217, 217, 217, 0.666); border-radius: 24px;">
@@ -37,49 +39,49 @@ document.getElementById("body-container").innerHTML=`<div class="loader" id="loa
                           </div>
                         </div>`;
 
-        await fetch("https://api.unsplash.com/search/photos/", configObj)
+        await fetch(`https://api.unsplash.com/search/photos?client_id=Lit6CmPbDpMfc6suXVJ9IdPibYV3TTxpWcazLE8wBQM&query=${search}&count=15`, configObj)
         .then(async function (response) {
           console.log(response);
           return await response.json();
         })
         .then(async function (object){
-        // {    
+         {    
           console.log(object);
-        //     let json=object;
-        //     console.log(json);
-        //     var a=document.getElementById("body-container").innerHTML="";
-        //     for(let i = 0; i < json.length; i++) {
-        //         let obj = json[i];
-        //         console.log(obj.image);
-        //        // console.log("hello");
-        //       console.log(json);
-        //       document.getElementById("body-container").innerHTML+= `<div class="cont">
-        //       <img
-        //         src="${obj.image}"
-        //         alt=""
-        //         height="300px"
-        //         srcset=""
-        //         class="image"
-        //       />
-        //       <div class="cont-disc">
-        //       <div class="cont-disc-name">${obj.title}</div>
-        //         <p>
-        //         ${obj.description}
-        //         </p>
-        //         <h4>by ${obj.name}</h4>
+            let json=object.results;
+            console.log(json);
+            var a=document.getElementById("body-container").innerHTML="";
+            for(let i = 0; i < json.length; i++) {
+                let obj = json[i];
+               // console.log(obj.image);
+               // console.log("hello");
+              console.log(json);
+              document.getElementById("body-container").innerHTML+= `<div class="cont">
+              <img
+                src="${obj.urls.regular}"
+                alt=""
+                height="300px"
+                srcset=""
+                class="image"
+              />
+              <div class="cont-disc">
+              <div class="cont-disc-name">${search}</div>
+                <p>
+                ${obj.description}
+                </p>
+                <h4>by ${obj.user.first_name}</h4>
       
-        //         <div class="btn-container">
-        //           <div href="" class="button1">♡</div>
-        //           <a href="${obj.image}" download="${obj.title}.jpg" class="button1">
-        //             <img
-        //               src="https://img.icons8.com/?size=512&id=Ezk6WeFucgyE&format=png"
-        //               width="25px"
-        //               height="25px"
-        //             />
-        //           </a>
-        //           <div href="" class="button1">➦</div>
-        //         </div>
-        //       </div>
-        //     </div>`;
-        //       console.log("end");}
-      });};
+                <div class="btn-container">
+                  <div href="" class="button1">♡</div>
+                  <a href="${obj.description}" download="${obj.search}.jpg" class="button1">
+                    <img
+                      src="https://img.icons8.com/?size=512&id=Ezk6WeFucgyE&format=png"
+                      width="25px"
+                      height="25px"
+                    />
+                  </a>
+                  <div href="" class="button1">➦</div>
+                </div>
+              </div>
+            </div>`;
+              console.log("end");}
+        };})};
